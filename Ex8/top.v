640885f6c7d36d1a9248afa5dc01a011665b6a0c
Cloud User <centos@ip-172-31-2-229.eu-west-2.compute.internal>
2020-06-10 11:24:32 +0000

    Ex8 complete

--- Ex8/top.v
+++ Ex8/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #8
-// Student Name:
-// Date: 
+// Student Name: Alex Goldie
+// Date: 10/6/2020
 //
 //  Description: In this exercise, you need to implement a times table of 0..7x0..7
 //  using a memory and AXI-4-lite interface.
@@ -12,3 +12,53 @@
 //  outputs:
 //           result[5:0]
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 100ps
+
+module times_table (
+	input clk,
+	input [2:0] a,
+	input [2:0] b,
+	input enable,
+	input rst,
+	output wire [5:0] result
+	);
+
+	wire [32:0] addr;
+	wire awready;
+	wire rsta_busy;
+	wire rstb_busy;
+	wire wready;
+	wire [1:0] bresp;
+	wire bvalid;
+	wire arready;
+	wire rvalid;
+	wire s_axi_rresp;
+	
+	
+	assign addr = {24'd0,a,b,2'd0};
+
+blk_mem_gen_0 Axi4_multiply (
+  .rsta_busy(rsta_busy),          // output wire rsta_busy
+  .rstb_busy(rstb_busy),          // output wire rstb_busy
+  .s_aclk(clk),                // input wire s_aclk
+  .s_aresetn(rst),          // input wire s_aresetn
+  .s_axi_awaddr(0),    // input wire [31 : 0] s_axi_awaddr
+  .s_axi_awvalid(0),  // input wire s_axi_awvalid
+  .s_axi_awready(awready),  // output wire s_axi_awready
+  .s_axi_wdata(0),      // input wire [31 : 0] s_axi_wdata
+  .s_axi_wstrb(0),      // input wire [3 : 0] s_axi_wstrb
+  .s_axi_wvalid(0),    // input wire s_axi_wvalid
+  .s_axi_wready(wready),    // output wire s_axi_wready
+  .s_axi_bresp(bresp),      // output wire [1 : 0] s_axi_bresp
+  .s_axi_bvalid(bvalid),    // output wire s_axi_bvalid
+  .s_axi_bready(0),    // input wire s_axi_bready
+  .s_axi_araddr(addr),    // input wire [31 : 0] s_axi_araddr
+  .s_axi_arvalid(1),  // input wire s_axi_arvalid
+  .s_axi_arready(arready),  // output wire s_axi_arready
+  .s_axi_rdata(result),      // output wire [31 : 0] s_axi_rdata
+  .s_axi_rresp(s_axi_rresp),      // output wire [1 : 0] s_axi_rresp
+  .s_axi_rvalid(rvalid),    // output wire s_axi_rvalid
+  .s_axi_rready(enable)    // input wire s_axi_rready
+);
+
+endmodule
--- Ex8/top_tb.v
+++ Ex8/top_tb.v
@@ -1,9 +1,105 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Test bench for Exercise #8 - Times Table using AXI interface
-// Student Name:
-// Date: 
+// Student Name: Alex Goldie
+// Date: 10/6/2020
 //
 // Description: A testbench module to test Ex8 - Times Table using AXI4-Lite
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
 
+`timescale 1ns / 100ps
+
+module top_tb(
+    );
+    
+    //Parameters
+    parameter CLK_PERIOD = 10;
+
+    //Registers and wires
+    reg clk;
+	reg [2:0] a;
+	reg [2:0] b;
+	reg enable;
+	wire [5:0] result;
+	reg err;
+	reg [5:0] result_prev;
+	reg rst;
+
+    //Clock generation
+    initial
+    begin
+       clk = 1'b0;
+       forever
+         #(CLK_PERIOD/2) clk=!clk;
+     end
+    
+ //Todo: User logic
+	initial begin
+       	err=0; 
+		enable = 1;
+		a = 0;
+		b = 0;
+		rst = 0;
+		#30
+		rst = 1;
+		result_prev = result;
+
+       forever begin
+       #30
+		if ((enable == 0) & (result != result_prev))
+		begin
+		
+           $display("***TEST FAILED! Enable didnt work! enable==%d, result==%d***", enable, result);
+           err=1;
+		end
+		
+		if ((enable == 1) & (result != a * b))
+		begin
+		
+           $display("***TEST FAILED! Multiply incorrect! result==%d, a==%d, b==%d***", result,a, b);
+           err=1;
+		end
+
+		if (a == 3'b010 & b == 3'b010)
+		enable = !enable;
+		if (a == 3'b011 & b == 3'b010)
+		enable = 1;
+
+		if (a<3'b111)
+		a = a+1;
+		else
+		a = 0;
+
+		result_prev = result;
+end
+end
+
+initial begin
+#34
+forever begin
+#240
+b = b+1;
+end
+end
+
+
+//Todo: Finish test, check for success
+      initial begin
+        #1970
+        if (err==0)
+          $display("***TEST PASSED! :) ***");
+        $finish;
+      end
+
+
+    //User's module
+    times_table top (
+	.clk (clk),
+	.enable (enable),
+	.a (a),
+	.b (b),
+	.result (result),
+	.rst (rst)
+     );
+     
+endmodule 
